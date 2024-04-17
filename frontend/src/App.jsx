3877aa09ce82301a37a1a948ab36fd9b02f11558
ArmanKukreti <armankukreti@gmail.com>
2024-04-17 15:52:25 +0530

    github api integrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,31 @@
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
-import ExplorePage from './pages/ExplorePage'
-import LikesPage from './pages/LikesPage'
-import Sidebar from './components/Sidebar'
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ExplorePage from "./pages/ExplorePage";
+import LikesPage from "./pages/LikesPage";
+import Sidebar from "./components/Sidebar";
+
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-
   return (
     <>
       <div className="flex">
-        <Sidebar/>
-        <div className='max-w-5xl m-5 text-white mx-auto transition-all duration-300 flex-1'>
+        <Sidebar />
+        <div className="max-w-5xl m-5 text-white mx-auto transition-all duration-300 flex-1">
           <Routes>
-            <Route path='/' element={<HomePage/>} />
-            <Route path='/login' element={<LoginPage/>} />
-            <Route path='/signup' element={<SignupPage/>} />
-            <Route path='/explore' element={<ExplorePage/>} />
-            <Route path='/Likes' element={<LikesPage/>} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/explore" element={<ExplorePage />} />
+            <Route path="/Likes" element={<LikesPage />} />
           </Routes>
+          <Toaster />
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
