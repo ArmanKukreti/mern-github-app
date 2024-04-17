6d1d6c7e9aae4a5220accf5e6591d6e34a465bdb
ArmanKukreti <armankukreti@gmail.com>
2024-04-18 02:03:18 +0530

    replaced the env credentials

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,26 +5,24 @@ export const getUserProfileAndRepos = async (req, res) => {
   const { username } = req.params;
 
   try {
-    //60 requests per hour, 5000 requests per hour for authenticated requests
-    const userRes = await axios.get(
-      `https://api.github.com/users/${username}`,
-      {
-        headers: {
-          Authorization: `token ${process.env.GITHUB_API_KEY}`,
-        },
-      }
-    );
-    const userProfile = userRes.data;
+    // 60 requests per hour, 5000 requests per hour for authenticated requests
+		// https://docs.github.com/en/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28
+		const userRes = await fetch(`https://api.github.com/users/${username}`, {
+			headers: {
+				authorization: `token ${process.env.GITHUB_API_KEY}`,
+			},
+		});
 
-    const repoRes = await axios.get(userProfile.repos_url, {
-      headers: {
-        Authorization: `token ${process.env.GITHUB_API_KEY}`,
-      },
-    });
-    const repos = repoRes.data;
-    
+		const userProfile = await userRes.json();
 
-    res.status(200).json({ userProfile, repos });
+		const repoRes = await fetch(userProfile.repos_url, {
+			headers: {
+				authorization: `token ${process.env.GITHUB_API_KEY}`,
+			},
+		});
+		const repos = await repoRes.json();
+
+		res.status(200).json({ userProfile, repos });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,46 @@
 import express from "express";
-import dotenv from 'dotenv';
-import cors from 'cors';
+import dotenv from "dotenv";
+import cors from "cors";
 import "./passport/github.auth.js";
-import session from 'express-session';
+import session from "express-session";
 
 import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
 import authRoutes from "./routes/auth.route.js";
-import passport from 'passport';
-
+import passport from "passport";
+import path from "path";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+console.log("dirname", __dirname);
+
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
+);
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors())
-
-app.get("/", (req, res) => {
-    res.send("Server is ready");
-})
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist"))); //for deployment
+
+app.get("*", (req, res) => { //for deployment
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 app.listen(5000, () => {
-  console.log("Server started on http://localhost:5000");
+  console.log(`Server started on http://localhost:${PORT}`);
   connectMongoDB();
 });
